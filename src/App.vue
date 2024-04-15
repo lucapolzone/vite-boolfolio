f3f9f9925eb13353df3047adf45deffb1c37d2e0
lucapolzone <lucapolzone@gmail.com>
2024-04-15 14:33:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    }
+  },
+
+
+
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<!-- @use '/src/scss/general.scss'; -->
+<style lang="scss">
+  @use './scss/general.scss';
+</style>
