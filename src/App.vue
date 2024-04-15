dc35adee96268aca27426ea84833089d3b133940
lucapolzone <lucapolzone@gmail.com>
2024-04-15 14:55:20 +0200

    feat: print list of laravel projects

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      title: "Hello world",
+      title: "Boolfolio Frontend",
+      projects: []
     }
   },
 
+  created() {
+    // console.log(axios);
+    axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+      this.projects = response.data.data;
+    });
 
+  }
 
 }
 
@@ -17,6 +24,14 @@ export default {
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
+
+    <div v-for="project in projects">
+      <ul>
+        <li><strong>ID: </strong>{{ project.id }}</li>
+        <li><strong>Titolo: </strong>{{ project.title }}</li>
+        <li><strong>Descrizione: </strong>{{ project.content }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
