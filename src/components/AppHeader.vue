e24ebb6adba2b2add20ca31d9d4ae5c3ab1034e3
lucapolzone <lucapolzone@gmail.com>
2024-04-16 15:14:14 +0200

    feat: add nav router links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,22 @@
 </script>
 
 <template>
-  <nav class="navbar">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">{{ title }}</a>
+      <a class="navbar-brand" href="#">{{ title }}</a> 
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'projects' }" class="nav-link">Projects</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
