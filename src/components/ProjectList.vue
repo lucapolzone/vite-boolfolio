adc855c8c42d32ed79d74d5441e6725e69009f9a
lucapolzone <lucapolzone@gmail.com>
2024-04-16 09:46:41 +0200

    feat: add pagination

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -8,24 +8,50 @@ import ProjectCard from './ProjectCard.vue';
     data() {
       return {
         store,
+        pagination: [],
       }
+    },
+
+    methods: {
+      fetchProjects(endpoint = api.baseUrl + 'projects') {
+        axios.get(endpoint).then((response) => {
+          store.projects = response.data.data;
+          this.pagination = response.data.links;
+        });
+      }
+
     },
 
     components: { ProjectCard },
 
     created() {
       // console.log(axios);
-      axios.get(api.baseUrl + 'projects').then((response) => {
-       store.projects = response.data.data;
-      });
+      this.fetchProjects();
     },
   }
 </script>
 
 <template>
-  <div class="row row-cols-3 g-3">
-    <project-card v-for="project in store.projects" :project="project" />
-  </div>
+
+  <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li 
+          v-for="link in pagination" 
+          @click="fetchProjects(link.url)" 
+          :class="{
+            active: link.active, 
+            disabled: !link.url, 
+          }"
+          class="page-item">
+          <a class="page-link" href="#" v-html="link.label"></a>
+        </li>
+      </ul>
+  </nav>
+
+    <div class="row row-cols-4 g-3">
+      <project-card v-for="project in store.projects" :project="project" />
+    </div>
+  
 </template>
 
 <style lang="scss" scoped>
