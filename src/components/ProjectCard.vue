6a982539abf1f60e476b2e6e8925593fd7421c62
lucapolzone <lucapolzone@gmail.com>
2024-04-22 13:47:22 +0200

    feat: project card title if is detail

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,15 +11,21 @@
 </script>
 <template>
   <div class="col">
+    <h1 v-if="isDetail" class="card-title">{{ project.title }}</h1>
+
       <div class="card h-100">
         <img :src="project.image ? project.image : 'https://placehold.co/600x400'" class="card-img-top">
         <div class="card-body">
-          <span :style="'background-color: ' + project.type.color" class="badge mb-2">{{ project.type.label }}</span>
+          <span :style="'background-color: ' + project.type.color" class="badge mb-2">
+            {{ project.type.label }}
+          </span>
           
           <h2 v-if="!isDetail" class="card-title h5">{{ project.title }}</h2>
-          <h1 v-else class="card-title">{{ project.title }}</h1>
           
-          <p class="card-text">{{ abstract }}</p>
+          <p class="card-text">
+            {{ abstract }}
+          </p>
+
           <router-link v-if="!isDetail" :to="{ name: 'projects.show', params: { id: project.id } }" class="btn btn-primary">
             Vedi altro
           </router-link>
